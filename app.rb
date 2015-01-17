bc611edc16123de4f629f06571c03625399dfc58
emmaladouceur <eladouceur@brearley.org>
2015-01-17 17:57:14 -0500

    working up to local host

--- app.rb
+++ app.rb
@@ -1,15 +1,12 @@
 require 'bundler'
 Bundler.require
 require './models/search'
-require 'sinatra/config_file'
 
 class MyApp < Sinatra::Base
-	register Sinatra::ConfigFile
 
-	config_file './config.yml'
 
 	get '/' do
-		# @twitter = Search.new
-		binding.pry
+		@twitter = Search.new
+		erb :index
 	end
 end
--- models/search.rb
+++ models/search.rb
@@ -1,12 +1,20 @@
+
+
 class Search
 
+	attr_accessor :client
+
 	def initialize
-		client = Twitter::REST::Client.new do |config|
-		  config.consumer_key = settings.TWITTER_CONSUMER_KEY
-		  config.consumer_secret = settings.TWITTER_CONSUMER_SECRET
-		  config.access_token = settings.TWITTER_ACCESS_TOKEN_KEY
-		  config.access_token_secret = settings.TWITTER_ACCESS_TOKEN_SECRET
+		@client = Twitter::REST::Client.new do |config|
+		  config.consumer_key = '6bg6YxHCZypX3SqrmRmhNhIJj'
+		  config.consumer_secret = 'ZtTaiReFpJQpef1SolUOV9lLoH5h6AAdY8T91ubDCD83qDH6pW'
+		  config.access_token = '2972123895-PrzpRYQSEfOHmTUW4p3kr5Dv5Sx95l1Hb4Uq4Kz'
+		  config.access_token_secret = 'uzVoqVqOdlzQuu2YaxtGnKoVrd62n1IXx1KaTdL0f30VZ'
 		end
+	end
+
+	def tweet_count
+		count = @client.search("#rubyonrails -rt", lang: "en").count
 	end
 
 end
