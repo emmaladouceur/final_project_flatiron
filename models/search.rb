499dab75af7103481354b7e207111575461410ec
emmaladouceur <eladouceur@brearley.org>
2015-01-24 15:23:26 -0500

    working online with results and home pages

--- models/search.rb
+++ models/search.rb
@@ -1,20 +1,20 @@
 
-
 class Search
 
 	attr_accessor :client
 
-	def initialize
+	def initialize(hashtag)
 		@client = Twitter::REST::Client.new do |config|
 		  config.consumer_key = '6bg6YxHCZypX3SqrmRmhNhIJj'
 		  config.consumer_secret = 'ZtTaiReFpJQpef1SolUOV9lLoH5h6AAdY8T91ubDCD83qDH6pW'
 		  config.access_token = '2972123895-PrzpRYQSEfOHmTUW4p3kr5Dv5Sx95l1Hb4Uq4Kz'
 		  config.access_token_secret = 'uzVoqVqOdlzQuu2YaxtGnKoVrd62n1IXx1KaTdL0f30VZ'
 		end
+		@hashtag = hashtag
 	end
 
 	def tweet_count
-		count = @client.search("#rubyonrails -rt", lang: "en").count
+		count = @client.search(@hashtag + " -rt", lang: "en").count
 	end
 
 end
